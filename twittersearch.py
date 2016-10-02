bb0249b9c86082b500646e78017f93e4bfe0ead5
Barbara Plank <b.plank@rug.nl>
2016-10-02 16:53:27 +0200

    added twittersearch sample

---
+++ twittersearch.py
@@ -0,0 +1,17 @@
+import twitter
+import json
+from twitter import Twitter, OAuth, TwitterStream
+
+cred = json.load(open("private/credentials-barbara.json"))
+# Initiate the connection to Twitter REST API
+
+my_authentication = OAuth(cred["ACCESS_TOKEN"], cred["ACCESS_TOKEN_SECRET"], \
+                          cred["CONSUMER_KEY"], cred["CONSUMER_SECRET"])
+
+twitter = Twitter(auth=my_authentication)
+            
+# Search for latest tweets about "#nlproc"
+query =  twitter.search.tweets(q='Groningen',count=10)
+for tweet in query["statuses"]:
+    print(tweet['text'])
+
